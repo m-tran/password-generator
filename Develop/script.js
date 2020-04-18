5f1a33d822a238b27f4ad21beefd944628e102b8
Michelle Tran <mptran0101@berkeley.edu>
2020-04-18 00:45:35 -0700

    modified prompts

--- Develop/script.js
+++ Develop/script.js
@@ -11,17 +11,26 @@ function writePassword() {
 }
 
 function generatePassword() {
+
+  //prompts
   var passwordLength = prompt("How many characters will you password be? Enter a number between 8 and 128.");
   if (passwordLength < 8 || passwordLength > 128) {
-    prompt("The number you entered is incorrect. Please enter a number between 8 and 128.");
+    prompt("We don't see that number here. Please enter a number between 8 and 128.");
   } else {
-    alert("Your password will be " + passwordLength + " characters.");
+    alert("We got it! Your password will be " + passwordLength + " characters.");
   }
-  var characterTypes = prompt("Enter at least one character type: lowercase, uppercase, numeric, and/or special characters. If selecting more than one character type, please separate each type with a comma.");
+  var characterTypes = prompt("Enter at least one of the following character types: lowercase, uppercase, numeric, and/or special characters.");
   if (characterTypes === "") {
-  prompt("Enter at least one character type: lowercase, uppercase, numeric, and/or special characters. If selecting more than one character type, please separate each type with a comma.");
+  prompt("Looks empty! Enter at least one of the following character types: lowercase, uppercase, numeric, and/or special characters.");
   } else {
-  alert("Your password will include the following character types: " + characterTypes);
+  alert("Gotcha. Your password will include the following character types: " + characterTypes + ".");
+  }
+
+  // character types
+  var lowercase
+
+  function passwordMix() {
+
   }
 }
 
